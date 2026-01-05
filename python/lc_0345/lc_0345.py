21b0bafc6089dca8520926d9daa8834e86b225bd
Sam Showalter <samshow@stripe.com>
2026-01-04 18:45:01 -0800

    finished for today

--- python/lc_0345/lc_0345.py
+++ python/lc_0345/lc_0345.py
@@ -6,6 +6,37 @@ from typing import Dict, List
 # Problem # 0345
 #######################################################################
 problem = """
+345. Reverse Vowels of a String
+Easy
+
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+Example 1:
+
+Input: s = "IceCreAm"
+
+Output: "AceCreIm"
+
+Explanation:
+
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+Example 2:
+
+Input: s = "leetcode"
+
+Output: "leotcede"
+
+ 
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consist of printable ASCII characters.
 """
 
 #######################################################################
@@ -13,10 +44,37 @@ problem = """
 #######################################################################
 
 notes = """
+Scan through the string and isolate vowels and their indices separately
+Then, reverse the vowels, but keep indices the same, and replace them all
 
+MISTAKE: s.split() does not split string, need list(s)
 """
 
 #######################################################################
 # Solution
 #######################################################################
 
+class Solution:
+    VOWELS = set({'a','e','i','o','u'})
+    def is_vowel(self, char: str):
+        return char.lower() in Solution.VOWELS
+        
+    def reverseVowels(self, s: str) -> str:
+        vowels: List[str] = []
+        vowel_inds: List[int] = []
+        s_list: List[str] = list(s)
+
+        for i, char in enumerate(s_list):
+            if self.is_vowel(char):
+                vowel_inds.append(i)
+                vowels.insert(0,char)
+        
+        for ind, v in zip(vowel_inds, vowels):
+            s_list[ind] = v
+            
+        return "".join(s_list)
+
+s = Solution()
+print(s.reverseVowels("leetcode"))
+
+        
--- python/lc_0605/lc_0605.py
+++ python/lc_0605/lc_0605.py
@@ -6,6 +6,32 @@ from typing import Dict, List
 # Problem # 0605
 #######################################################################
 problem = """
+605. Can Place Flowers
+
+Easy
+
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
 """
 
 #######################################################################
@@ -13,10 +39,38 @@ problem = """
 #######################################################################
 
 notes = """
+Find first spot where there is nothing around you, then hop 2x each time as you plant
+[1,0,0,0,0,1]
 
+Correct first try
 """
 
 #######################################################################
 # Solution
 #######################################################################
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        bed_len = len(flowerbed)
+        potential_beds = 0
+        for i,f in enumerate(flowerbed):
+            can_bed = (
+                f == 0 # Must be empty in its position
+                and (
+                    i == 0 or flowerbed[i-1] == 0
+                ) and (
+                    i + 1 == bed_len or flowerbed[i+1] == 0
+                )
+            )
+            if can_bed:
+                flowerbed[i] = 1
+                potential_beds += 1
+
+        return n <= potential_beds
+
+flowerbed = [1,0,0,0,0,0,1]
+n = 2
+s = Solution()
+print(s.canPlaceFlowers(flowerbed, n))
+        
 
