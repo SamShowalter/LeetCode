abbd838a9e0037e4212b08af9cbdeb03eff1d6d5
Samuel Showalter <samuelrshowalter@gmail.com>
2026-01-05 22:59:23 -0800

    finished the string ones

--- python/lc_0334/lc_0334.py
+++ python/lc_0334/lc_0334.py
@@ -6,6 +6,37 @@ from typing import Dict, List
 # Problem # 0334
 #######################################################################
 problem = """
+334. Increasing Triplet Subsequence
+Medium
+
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+Example 2:
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+Example 3:
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: One of the valid triplet is (1, 4, 5), because nums[1] == 1 < nums[4] == 4 < nums[5] == 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
 """
 
 #######################################################################
@@ -13,10 +44,50 @@ problem = """
 #######################################################################
 
 notes = """
+Technically just running out the pointers in O(n) time
+- when arr[i] >= arr[j], move everything forward
+- when arr[j] >= arr[k], just move j and k forward
 
+VERY CHALLENGING
+- Less than or equal to is essential to ensure things are not equal
+- Second smallest must be > smallest
+- The order or assignment also really matters
+
+Took 3 Submission tries and a lot of thinking
 """
 
 #######################################################################
 # Solution
 #######################################################################
 
+class Solution:
+    def increasingTriplet(self, arr: List[int]) -> bool:
+        arrlen = len(arr)
+        if arrlen < 3: return False
+        
+        smallest = float("inf")
+        second_smallest = float("inf")
+        # [2,1,5,0,4,6]
+
+        for i in range(arrlen):
+            if arr[i] <= smallest:
+                smallest = arr[i]
+            elif arr[i] <= second_smallest:
+                second_smallest = arr[i]
+            elif second_smallest > smallest:
+                return True
+
+        return False
+                
+            
+
+s = Solution()
+arr = [1,1,1,1,1]
+"""
+20,100
+20,100
+
+
+
+"""
+print(s.increasingTriplet(arr))
--- python/lc_0443/lc_0443.py
+++ python/lc_0443/lc_0443.py
@@ -6,6 +6,46 @@ from typing import Dict, List
 # Problem # 0443
 #######################################################################
 problem = """
+443. String Compression
+Medium
+
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+If the group's length is 1, append the character to s.
+Otherwise, append the character followed by the group's length.
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+
+You must write an algorithm that uses only constant extra space.
+
+Note: The characters in the array beyond the returned length do not matter and should be ignored.
+
+ 
+
+Example 1:
+
+Input: chars = ["a","a","b","b","c","c","c"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+Example 2:
+
+Input: chars = ["a"]
+Output: Return 1, and the first character of the input array should be: ["a"]
+Explanation: The only group is "a", which remains uncompressed since it's a single character.
+Example 3:
+
+Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+ 
+
+Constraints:
+
+1 <= chars.length <= 2000
+chars[i] is a lowercase English letter, uppercase English letter, digit, or symbol.
 """
 
 #######################################################################
@@ -13,10 +53,46 @@ problem = """
 #######################################################################
 
 notes = """
-
 """
 
 #######################################################################
 # Solution
 #######################################################################
 
+class Solution:
+    def update_chars(self, chars, update_index, char, cnt) -> int:
+        chars[update_index] = char
+        update_index += 1
+        if cnt > 1:
+            str_cnt = str(cnt)
+            for num_char in str_cnt:
+                chars[update_index] = num_char
+                update_index += 1
+        return update_index
+                     
+    def compress(self, chars: List[str]) -> int:
+        cnt = 1
+        char = chars[0]
+        update_index = 0
+
+        for i in range(1,len(chars)):
+            if chars[i] != char:
+                update_index = self.update_chars(chars, update_index, char, cnt)
+                cnt = 1
+                char = chars[i]
+            else:
+                cnt += 1
+
+        update_index = self.update_chars(chars, update_index, char,cnt)
+        # Don't need +1 since it will add one for me
+        return update_index
+
+chars = ["a","a","b","b","c","c","c"]
+print(chars)
+s = Solution()
+print(s.compress(chars))
+                
+
+            
+            
+            
