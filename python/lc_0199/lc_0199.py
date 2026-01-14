40a322d6f92fad093bd8ca4a191c419abb6c036c
Samuel Showalter <samuelrshowalter@gmail.com>
2026-01-13 22:04:56 -0800

    more provlems

--- python/lc_0199/lc_0199.py
+++ python/lc_0199/lc_0199.py
@@ -3,7 +3,7 @@ import sys
 from typing import Dict, List
 
 #######################################################################
-# Problem # 0199 2026-01-12 Start time: End time:
+# Problem # 0199 2026-01-13 Start time: 21:38 End time: 21:47
 #######################################################################
 problem = """
 199. Binary Tree Right Side View
@@ -60,9 +60,40 @@ The number of nodes in the tree is in the range [0, 100].
 notes = """
 Track the depth and the left-ness
 
+OR
+
+Use BFS (simpler)
+
+MISTAKE: 
+- Did not include the case where the root node is null
+- Did not actually include a return statement
+
 """
 
 #######################################################################
 # Solution
 #######################################################################
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+        right_side_view: List[int] = []
+        queue = [root]
+        next_level = []
+        while len(queue) > 0:
+            node = queue.pop()
+            if node.left:
+                next_level.insert(0,node.left)
+            if node.right:
+                next_level.insert(0,node.right)
+            
+            if len(queue) == 0:
+                right_side_view.append(node.val)
+                queue = next_level
+                next_level = []
+        return right_side_view
+            
+            
+            
 
--- python/lc_0700/lc_0700.py
+++ python/lc_0700/lc_0700.py
@@ -3,9 +3,36 @@ import sys
 from typing import Dict, List
 
 #######################################################################
-# Problem # 0700
+# Problem # 0700 2026-01-13 Start time: 22:00 End time: 
 #######################################################################
 problem = """
+700. Search in a Binary Search Tree
+Easy
+
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+Example 2:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 5000].
+1 <= Node.val <= 107
+root is a binary search tree.
+1 <= val <= 107
 """
 
 #######################################################################
@@ -13,10 +40,23 @@ problem = """
 #######################################################################
 
 notes = """
-
+Simple BST, can use recursion
+- Flipped the sign of the conditions at first, silly error
 """
 
 #######################################################################
 # Solution
 #######################################################################
 
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root == None:
+            return None
+        if root.val == val:
+            return root
+        # Silly mistake, flipped the sign
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+            
